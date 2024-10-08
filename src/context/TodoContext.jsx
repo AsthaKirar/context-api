08ed2fd832689c926586b0dff37ebff7944c06d0
AsthaKirar <asthap602@gmail.com>
2024-10-08 23:19:13 +0530

    commit

---
+++ src/context/TodoContext.jsx
@@ -0,0 +1,11 @@
+import {createContext} from 'react'
+export const todoContext = createContext(null);
+
+const TodoContext = (props) => {
+    console.log(props)
+  return (
+    
+  )
+}
+
+export default TodoContext
